441eaf220217586dc98b5a62c8e3d4fd3eb3d9ec
Ali Mousazada <mousazada.ali@gmail.com>
2024-03-31 16:18:40 -0700

    adding lasr part of jacaScript Code

--- app.js
+++ app.js
@@ -13,5 +13,27 @@ function addTask() {
     li.appendChild(span);
   }
   inputBox.value = "";
+  saveData();
 }
 
+lsitContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+        
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+        
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data", lsitContainer.innerHTML);
+}
+
+function showTask(){
+    lsitContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
