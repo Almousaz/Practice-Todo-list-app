c7776d2947c667972c915d880be7057ec7404623
Ali Mousazada <mousazada.ali@gmail.com>
2024-03-31 15:54:32 -0700

    adding javascript code

---
+++ app.js
@@ -0,0 +1,17 @@
+const inputBox = document.getElementById("input-box");
+const lsitContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value ;
+    lsitContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+}
+
